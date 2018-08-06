43cf67691b6be59e9f615558e1c66414a3c921b8
Bartosz Piatek <piatekbart@gmail.com>
2018-08-06 22:32:41 +0200

    added Student model

---
+++ src/main/java/pl/bartaspol/demo/model/Student.java
@@ -0,0 +1,13 @@
+package pl.bartaspol.demo.model;
+
+import lombok.Data;
+
+/**
+ * Created by Bartosz Piatek on 06/08/2018
+ */
+@Data
+public class Student {
+
+    private String name;
+    private String tel;
+}
