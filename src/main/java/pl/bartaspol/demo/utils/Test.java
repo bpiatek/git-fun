40d3ff898a5d223a2188da55ce2bc518154fe30b
Bartosz Piatek <piatekbart@gmail.com>
2018-08-06 22:30:28 +0200

    added Test class

---
+++ src/main/java/pl/bartaspol/demo/utils/Test.java
@@ -0,0 +1,10 @@
+package pl.bartaspol.demo.utils;
+
+/**
+ * Created by Bartosz Piatek on 06/08/2018
+ */
+public class Test {
+    public void print() {
+        System.out.println("dupa");
+    }
+}
